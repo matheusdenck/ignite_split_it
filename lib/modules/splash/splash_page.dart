07fde77ef9cb647c68d36b5a3cd57b78f8284edd
matheusdenck <65082832+matheusdenck@users.noreply.github.com>
2021-05-27 09:18:40 -0300

    .

--- lib/modules/splash/splash_page.dart
+++ lib/modules/splash/splash_page.dart
@@ -1,10 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:split_it/theme/app_theme.dart';
 
 class SplashPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('Splash page')),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: AppTheme.gradients.background,
+        ),
+        child: Column(
+          children: [
+            Image.asset(
+              'assets/images/retangulo-dir.png',
+              width: 198,
+              height: 98,
+            ),
+            Image.asset(
+              'assets/images/retangulo-dir.png',
+              width: 114,
+              height: 58,
+            ),
+            Image.asset(
+              'assets/images/logo.png',
+              width: 128,
+              height: 112,
+            ),
+            Image.asset(
+              'assets/images/retangulo-esq.png',
+              width: 114,
+              height: 58,
+            ),
+            Image.asset(
+              'assets/images/retangulo-esq.png',
+              width: 198,
+              height: 98,
+            )
+          ],
+        ),
+      ),
     );
   }
 }
